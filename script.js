84dc499746dfe079f02fc889d519c664ea13a024
Carly Peyton <carlypeyton@gmail.com>
2021-01-05 12:26:05 -0500

    Create JQuery document ready function. Create click event function for save button. When user clicks save button, the description for the specific time block is saved to local storage.

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var currentDay = moment().format("MMM Do" + ", YYYY");
 $("#currentDay").append(currentDay);
 var currentTime = moment().format("LT");
 $("#currentTime").append(currentTime);
+
+//JQuery document ready function
+$(document).ready(function () {
+  // Save button function
+  $(".saveBtn").on("click", function () {
+    // On click, get values of the description and time
+    var description = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    // Save description in local storage for that specific time
+    localStorage.setItem(time, description);
+  });
+});
