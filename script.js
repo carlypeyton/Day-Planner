fe45adf24e145c9bdfd79a98523f0580cb95879f
Carly Peyton <carlypeyton@gmail.com>
2021-01-05 12:52:39 -0500

    Create function to track the time. Use moment().hour() to determine current hour. For each item with time-block class, split the ID, to take the number from each time-block ID, and parse to integer to compare with the current hour. If the value of the time block is greater than the value of the current hour, than that time-block is in the future, and should be styled using the CSS for future class. If the value of the time-block is equal to that of the current hour, then that time-block is present, and should be styled using CSS for present class. Otherwise, if the value of the timeblock is less than the value of the current hour, it is in the past and should be styled accordingly. Need to add JQuery to get descriptions for calendars that are saved in local storage, if any are saved, and display. Call on function to track the current time.

--- script.js
+++ script.js
@@ -14,4 +14,39 @@ $(document).ready(function () {
     // Save description in local storage for that specific time
     localStorage.setItem(time, description);
   });
+  //Function to track the current time and display color blocks on page accordingly
+  function trackTime() {
+    //Get current hour
+    var currentHour = moment().hour();
+    //Then loop over time blocks to determine what class each block belongs to
+    //based on the current hour
+    $(".time-block").each(function () {
+      //For each time block, split ID at hour, parse string to change hour# to integer
+      //for comparison with the current hour
+      var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+      //If timeblock is less than current hour, PAST
+      if (timeBlock > currentHour) {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      }
+      //If timeblock is equal to current hour, PRESENT
+      else if (timeBlock === currentHour) {
+        $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+      }
+      //If timeblock is greater than current hour, FUTURE
+      else {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      }
+    });
+    //Need to get descriptions saved in local storage for each time
+  }
+  // Get descriptions from local storage if they exist for hours 0 to 23
+
+  // Call track time function to display color blocks
+  trackTime();
 });
