47b365dec28c69b93dfeba6f91f0ddd83c1ef22a
Carly Peyton <carlypeyton@gmail.com>
2021-01-05 12:55:01 -0500

    Use JQuery to get description for each time-block hour ID and set as value, so that any saved tasks appear when user goes to day planner, even after refreshing page. Need to run through validators and add CSS styling.

--- script.js
+++ script.js
@@ -43,10 +43,32 @@ $(document).ready(function () {
         $(this).removeClass("future");
       }
     });
-    //Need to get descriptions saved in local storage for each time
   }
   // Get descriptions from local storage if they exist for hours 0 to 23
-
+  $("#hour0 .description").val(localStorage.getItem("hour0"));
+  $("#hour1 .description").val(localStorage.getItem("hour1"));
+  $("#hour2 .description").val(localStorage.getItem("hour2"));
+  $("#hour3 .description").val(localStorage.getItem("hour3"));
+  $("#hour4 .description").val(localStorage.getItem("hour4"));
+  $("#hour5 .description").val(localStorage.getItem("hour5"));
+  $("#hour6 .description").val(localStorage.getItem("hour6"));
+  $("#hour7 .description").val(localStorage.getItem("hour7"));
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+  $("#hour18 .description").val(localStorage.getItem("hour18"));
+  $("#hour19 .description").val(localStorage.getItem("hour19"));
+  $("#hour20 .description").val(localStorage.getItem("hour20"));
+  $("#hour21 .description").val(localStorage.getItem("hour21"));
+  $("#hour22 .description").val(localStorage.getItem("hour22"));
+  $("#hour23 .description").val(localStorage.getItem("hour23"));
   // Call track time function to display color blocks
   trackTime();
 });
